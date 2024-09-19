7efac8bc596cacfb50bcc5ffd9e1460c4b57786c
Mateusz <pawelkomateusz@gmail.com>
2024-09-20 01:05:45 +0200

    fixes and started developing new features

--- Framework/DiscordHandler.py
+++ Framework/DiscordHandler.py
@@ -1,3 +1,4 @@
+import requests
 import websocket
 import json
 
@@ -5,6 +6,7 @@ from Framework.PacketFactory import PacketFactory
 from Framework.Logger import Logger, LogType
 from Framework.Bot import Bot
 from Framework.HeartbeatManager import HeartbeatManager
+from Framework.Commands.SlashCommand import SlashCommand
 
 class DiscordHandler:
 
@@ -18,10 +20,10 @@ class DiscordHandler:
 
         self._lastSequence = None
         self.event: str = ""
-        self.heartbeatIntervalMiliseconds: int = 0
+        self.heartbeatIntervalMilliseconds: int = 0
         self.sessionID = ""
         self.resumeURL = ""
-
+        self.commands: list[SlashCommand] = []
 
 
 
@@ -29,13 +31,30 @@ class DiscordHandler:
         # Connect to Discord and receive Hello Event
         self._connectToGateway()
         # Start heart beating thread.
-        self._startHeartbeat(self.heartbeatIntervalMiliseconds)
+        self._startHeartbeat(self.heartbeatIntervalMilliseconds)
         # Send Identify
         self._identify()
 
         # Upload Commands.
+        self._setupCommands()
 
+        # Start autohandler loop.
+    def _autohandler(self):
+        while True:
+            packet = self.receiveResponse()
+            # try to auto handle response
+            # if handler couldn't handle
 
+    # loads commands and sends them off to discord
+    def _setupCommands(self):
+        url: str = f"https://discord.com/api/v10/applications/{self._bot.appid}/commands"
+        headers: dict = {"Authorization": f"Bot {self._bot.getToken()}"}
+        command: SlashCommand
+        for command in self._bot.commands:
+            r = requests.post(url, headers=headers, json=command.setupPacket)
+            self._logger.print(f"Loaded Command {command.name} with status: {r.status_code}\n {r.json()}")
+            if r.status_code == 201:
+                self.commands.append(command)
 
     # function handles identifying and receiving Ready event.
     def _identify(self):
@@ -45,8 +64,8 @@ class DiscordHandler:
         self._logger.print("[Identify] Receiving READY event")
         self.handleResponse()
 
-    def _startHeartbeat(self, intervalMiliseconds):
-        self._heartBeatManager: HeartbeatManager = HeartbeatManager(intervalMiliseconds, self._websocket, self._logger)
+    def _startHeartbeat(self, intervalMilliseconds: int):
+        self._heartBeatManager: HeartbeatManager = HeartbeatManager(intervalMilliseconds, self._websocket, self._logger)
 
     # function handles connecting to gateway and receiving Hello Packet.
     def _connectToGateway(self):
@@ -58,56 +77,61 @@ class DiscordHandler:
             breakpoint()
         self._logger.print("[Connect To Gateway] Successfully connected to Discord's Gateway")
 
-
-
     # Returns TRUE if response was handled automatically
     # Returns FALSE if program couldn't handle response.
     def handleResponse(self) -> [bool, dict]:
-        waitForPacket: bool = True
-        while waitForPacket:
-            response: dict = self.receiveResponse()
-            match response.get('op', -1):
-                case -1:
-                    # program occurred error so return false
-                    self._logger.print(f"[Handler Error] Couldn't auto-handle response!\n Response:\n{response}\n----")
+        response: dict = self.receiveResponse()
+        match response.get('op', -1):
+            case -1:
+                # program occurred error so return false
+                self._logger.print(f"[Handler Error] Couldn't auto-handle response!\n Response:\n{response}\n----")
+                breakpoint()
+                return [False, response]
+            case 7:
+                # TODO OP 07
+                self._logger.print(f"[Handler] Gateway requested connection resume")
+                self._websocket.connect('wss://gateway.discord.gg/?v=6&encoding=json')
+                self._websocket.send(
+                    json.dumps(
+                        PacketFactory.resumePacket(self._bot.getToken(), self.sessionID, self._lastSequence)
+                    )
+                )
+                raise NotImplementedError()
+            case 10:
+                self._logger.print("Received HELLO event.")
+                # Hello Event
+                try:
+                    self.heartbeatIntervalMilliseconds = response.get("d").get("heartbeat_interval")
+                    return [True, {}]
+                except AttributeError as e:
+                    self._logger.print("chuj mniue jasny strzeli\n" + str(e))
                     breakpoint()
                     return [False, response]
-                case 7:
-                    # TODO OP 07
-                    self._logger.print(f"[Handler] Gateway requested connection resume")
-                    self._websocket.connect('wss://gateway.discord.gg/?v=6&encoding=json')
-                    self._websocket.send(
-                        json.dumps(
-                            PacketFactory.resumePacket(self._bot.getToken(), self.sessionID, self._lastSequence)
-                        )
-                    )
-                    raise NotImplementedError()
-                case 10:
-                    self._logger.print("Received HELLO event.")
-                    # Hello Event
-                    try:
-                        self.heartbeatIntervalMiliseconds = response.get("d").get("heartbeat_interval")
-                        return [True, {}]
-                    except AttributeError as e:
-                        self._logger.print("chuj mniue jasny strzeli\n" + str(e))
-                        breakpoint()
-                        return [False, response]
 
-                case 0:
-                    self._logger.print("Received READY event.")
-                    # Ready event
-                    try:
-                        self.sessionID = response.get("d").get("session_id")
-                        self.resumeURL = response.get("d").get("resume_url")
-                    except AttributeError as e:
-                        self._logger.print("AT READY: chuj mniue jasny strzeli\n" + str(e))
-                        breakpoint()
-                        return [False, response]
-                    self._logger.print("Sucessfully parsed READY event")
-                    return [True, {}]
-                case 11:
-                    self._logger.print("Heartbeat received")
+            case 0:
+                self._logger.print("Received READY event.")
+                # Ready event
+                try:
+                    self.sessionID = response.get("d").get("session_id")
+                    self.resumeURL = response.get("d").get("resume_url")
+                except AttributeError as e:
+                    self._logger.print("AT READY: chuj mniue jasny strzeli\n" + str(e))
+                    breakpoint()
+                    return [False, response]
+                self._logger.print("Sucessfully parsed READY event")
+                return [True, {}]
+            case 11:
+                self._logger.print("Heartbeat received")
 
+        match response.get("t", "nula"):
+            case "nula":
+                self._logger.print("Error occurred while trying to parse type event response!", logType=LogType.ERROR)
+            ### TODO: write command handler!
+            case "INTERACTION_CREATE":
+                self._handleCommand(response)
+
+    def _handleCommand(self, response: dict):
+        ...
     def receiveResponse(self) -> dict:
         response = None
         try:
--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-
-
-
-
 from Framework import DiscordHandler as discord
 from Framework import Bot as app
+import PingCommand
+
+# TODO: Remove token on public release!
+TOKEN = "MTI4NjEzNDQyNzg1MTgxNzAzMg.GWsWE0.JnxYw5Yr7b3cC7tbWEHiOC9SpxoD9cfVK_Mavo"
 
 
+pingCommand: PingCommand = PingCommand.PingCommand()
 
-TOKEN = "MTI3NjE5NTkzNTkzMTg1OTEyOA.GTcJNz.cPNBUy693oX_jdm32OKVJ1rwgCxUc5eugvgJK0"
-
-handler: discord.DiscordHandler = discord.DiscordHandler(app.Bot(TOKEN, 1276195935931859128))
+handler: discord.DiscordHandler = discord.DiscordHandler(
+    app.Bot(
+        TOKEN,
+        1286134427851817032,
+        [pingCommand]
+    )
+)
