363ab36e2378a15827c23fdbbb525a84df07d970
Mateusz <pawelkomateusz@gmail.com>
2024-09-20 23:54:31 +0200

    Added "Graj" slash command.

--- GameCommand.py
+++ GameCommand.py
@@ -1,6 +1,34 @@
+import json
+import time
+
 from Framework.Commands import SlashCommand
-
-
-class GameCommand(SlashCommand):
+import requests
+import random
+import re
+class GameCommand(SlashCommand.SlashCommand):
     def __init__(self):
-        super(GameCommand, self).__init__()
+        super(GameCommand, self).__init__("Graj", "Rozpocznij grę w cytaty!")
+    @staticmethod
+    def on_use(interactionToken: str, interactionID: str, token: str):
+        callbackURL = f"https://discord.com/api/v10/interactions/{interactionID}/{interactionToken}/callback"
+        channelMessages = requests.get("https://discord.com/api/v10/channels/1012458745248358461/messages",
+                                       headers={"Authorization": f"Bot {token}"})
+
+        channelMessages = json.loads(channelMessages.text)
+        message: str = ""
+        while True:
+            message = random.choice(channelMessages)["content"]
+            if message.count("~") == 2:
+                break
+        user: str = re.search(r'<@(\d+)>', message).group(0)
+        idx1 = message.index("~")
+        idx2 = message.rindex("~")
+        extractedMessage: str = ""
+        for idx in range(idx1+1, idx2):
+            extractedMessage += message[idx]
+        r = requests.post(callbackURL, headers={"Content-Type": "application/json"}, json={
+            "type":4,
+            "data": {
+                "content": f"Kto to powiedział?\n\"***{extractedMessage.strip()}***\"\nOdpowiedź to: ||{user}||"
+            }
+        })
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from Framework import DiscordHandler as discord
 from Framework import Bot as app
 import PingCommand
 import LeaderboardCommand
+from GameCommand import GameCommand
 
 # TODO: Remove token on public release!
 TOKEN = "MTI4NjEzNDQyNzg1MTgxNzAzMg.GWsWE0.JnxYw5Yr7b3cC7tbWEHiOC9SpxoD9cfVK_Mavo" # NOQA
@@ -9,11 +10,14 @@ TOKEN = "MTI4NjEzNDQyNzg1MTgxNzAzMg.GWsWE0.JnxYw5Yr7b3cC7tbWEHiOC9SpxoD9cfVK_Mav
 
 pingCommand: PingCommand = PingCommand.PingCommand()
 leaderboardCommand: LeaderboardCommand = LeaderboardCommand.LeaderboardCommand()
+gameCommand: GameCommand = GameCommand()
 handler: discord.DiscordHandler = discord.DiscordHandler(
     app.Bot(
         TOKEN,
         1286134427851817032,
         {leaderboardCommand.name: leaderboardCommand,
-                    pingCommand.name: pingCommand}
+                    pingCommand.name: pingCommand,
+                    gameCommand.name: gameCommand
+        }
     )
 )
